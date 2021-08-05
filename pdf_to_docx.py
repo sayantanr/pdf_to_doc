7ed9dab9ff609461dd706ae9d1eb85d5bc0caca7
SAYANTAN ROY <54001854+sayantanr@users.noreply.github.com>
2021-08-05 11:15:15 +0530

    Create pdf_to_docx.py

---
+++ pdf_to_docx.py
@@ -0,0 +1,23 @@
+from pdf2docx import parse
+
+pdf_file = '/path/to/sample.pdf'
+docx_file = 'path/to/sample.docx'
+
+# convert pdf to docx
+parse(pdf_file, docx_file)
+"""
+from pdf2docx import Converter
+
+pdf_file = '/path/to/sample.pdf'
+docx_file = 'path/to/sample.docx'
+
+# convert pdf to docx
+cv = Converter(pdf_file)
+cv.convert(docx_file)      # all pages by default
+cv.close()
+
+"""
+
+
+
+
